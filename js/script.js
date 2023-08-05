3083a7be3f99c4f4918c0104a0cedee71d8b7df8
abhishek sharma <abhi365ddd@gmail.com>
2023-08-05 17:04:28 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$('.insip-carousel .owl-carousel').owlCarousel({
+    // loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+
+$('.brand-carousel .owl-carousel').owlCarousel({
+    // loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
