46db46913498c363d1c4dfa54cd965b99fb8955d
abhishek sharma <abhi365ddd@gmail.com>
2023-11-25 13:55:52 +0530

    second commit

--- js/script.js
+++ js/script.js
@@ -1,35 +1,43 @@
-$('.insip-carousel .owl-carousel').owlCarousel({
-    // loop:true,
-    margin:10,
-    nav:true,
-    dots:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:6
-        }
-    }
-})
+const humberger = document.querySelector(".humberger");
+const menu = document.querySelector(".menu");
 
-$('.brand-carousel .owl-carousel').owlCarousel({
-    // loop:true,
-    margin:10,
-    nav:true,
-    dots:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:6
-        }
-    }
-})
+humberger.addEventListener("click", () => {
+  humberger.classList.toggle("active");
+  menu.classList.toggle("active");
+});
+
+$(".insip-carousel .owl-carousel").owlCarousel({
+  // loop:true,
+  margin: 10,
+  nav: true,
+  dots: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 6,
+    },
+  },
+});
+
+$(".brand-carousel .owl-carousel").owlCarousel({
+  // loop:true,
+  margin: 10,
+  nav: true,
+  dots: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 6,
+    },
+  },
+});
